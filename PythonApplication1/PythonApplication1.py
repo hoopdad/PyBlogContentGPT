8e6526ad7dbb45701a9d8d3557f41faf6133fd18
mike <mike@BOSGAME>
2023-10-19 21:19:27 -0400

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,149 @@
+import openai
+import requests
+import os
+import sys
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+
+actor=sys.argv[1]
+
+my_engine="text-davinci-002"
+my_engine="gpt-3.5"
+my_engine="gpt-3.5-turbo"
+my_engine="gpt-4"
+my_max_tokens=3000  # You can adjust this based on the desired length of the response
+
+# Title for the blog post
+title = sys.argv[2]
+# Prompt for content generation
+prompt = "Generate a blog post in HTML syntax about "+title+", including sections for an overview, key uses and benefits, a path to expertise, links with HTML link syntax to popular sites with training materials, and links with HTML link syntax to sites with relevant certifications or products. Each section should use an HTML H2 section title and lists of links should use HTML UL. Include an Attribution section at the end that identifies this content as being created by ChatGPT and a human prompt engineer who has more than 25 years of experience in upskilling himself. Add keywords to help search engines find this post. Make sure to use HTML formatting as much as possible."
+prompt = "Generate a blog post in HTML syntax about "+title+", including sections for an overview, key uses and benefits, what to look for, what to watch out for, links with HTML link syntax to popular products, and who are customers of those products, . Each section should use an HTML H2 section title and lists of links should use HTML UL. Include an Attribution section at the end that identifies this content as being created by ChatGPT and a human prompt engineer who has more than 25 years of experience in upskilling himself. Add keywords to help search engines find this post. Make sure to use HTML formatting as much as possible."
+
+# Define the scope for the Blogger API
+SCOPES = ['https://www.googleapis.com/auth/blogger']
+
+# Set your OpenAI GPT-3 API key
+openai.api_key = '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'
+
+# Set your Blogger API key and blog ID
+BLOGGER_API_KEY = 'AIzaSyDIfdKy0Fkv9muCl11axQDuVKahzGxxLYY'
+BLOG_ID = '4009390686325870720'
+
+def authenticate_blogger():
+    credentials = authenticate_with_google()
+    #print("credentials")
+    #print(credentials)
+
+    headers = {
+        'Authorization': f'Bearer {credentials.token}',
+        'Content-Type': 'application/json'
+    }
+    return headers
+
+# Function to authenticate with OAuth for a Google service
+def authenticate_with_google():
+    # The file token.json stores the user's access and refresh tokens and is
+    # created automatically when the authorization flow completes for the first time.
+    creds = None
+
+    # Load existing credentials from the file if available
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json')
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+# Function to generate content using OpenAI GPT-3
+def generate_content(prompt):
+    #response = openai.Completion.create(
+        #engine=my_engine,
+        #prompt=prompt,
+        #max_tokens=my_max_tokens
+    #)
+    #return response.choices[0].text.strip()
+    response = openai.ChatCompletion.create(
+        model=my_engine,
+        messages=[{"role":"system","content":actor},
+                  {"role":"user","content":prompt}
+        ])
+    return response["choices"][0]["message"]["content"]
+
+# Function to create a new blog post on Blogger.com
+def create_blog_post(title, content):
+    url = f'https://www.googleapis.com/blogger/v3/blogs/{BLOG_ID}/posts/'
+    
+    # Example post data
+    post_data = {
+        'kind': 'blogger#post',
+        'blog': {'id': BLOG_ID},
+        'title': title,
+        'content': content,
+    }
+    headers = authenticate_blogger()
+    #print("****URL****")
+    #print(url)
+    #print("****Headers****")
+    #print(headers)
+    #print("****POST DATA****")
+    #print(post_data)
+    response = requests.post(url, headers=headers, json=post_data)
+
+    if response.status_code == 200:
+        post_id = response.json().get('id')
+        return post_id
+    else:
+        print(f"Failed to create a blog post. Status Code: {response.status_code}")
+        return None
+
+# Function to publish a blog post on Blogger.com
+def publish_blog_post(post_id):
+    url = f'https://www.googleapis.com/blogger/v3/blogs/{BLOG_ID}/posts/{post_id}/publish?key={BLOGGER_API_KEY}'
+
+    headers = authenticate_blogger()
+
+    #print("****URL****")
+    #print(url)
+    #print("****Headers****")
+    #print(headers)
+    #print("****POST_ID****")
+    #print(post_id)
+
+    response = requests.post(url, headers=headers)
+
+    if response.status_code == 200:
+        print("Blog post published successfully.")
+    else:
+        print(f"Failed to publish the blog post. Status Code: {response.status_code}")
+
+# Main function to generate content and create/publish the blog post
+def main():
+
+    # Generate content using OpenAI GPT-3
+    generated_content = generate_content(prompt)
+
+    # Combine the generated content with additional information
+    full_content = f"{generated_content}"
+
+    # Create and publish the blog post
+    post_id = create_blog_post(title, full_content, )
+
+    if post_id:
+        # Publish the blog post
+        publish_blog_post(post_id)
+
+if __name__ == "__main__":
+    main()
+
